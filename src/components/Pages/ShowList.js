134aa52ed307097ae294be36982afcc792924505
drrinc <qkdghlqur3141@gmail.com>
2024-11-14 16:34:23 +0900

    </BrowserRouter>

--- src/components/Pages/ShowList.js
+++ src/components/Pages/ShowList.js
@@ -5,35 +5,37 @@ import URLhdl from "../Common/Handler";
 function ShowList() {
     const handler = URLhdl();
     return (
-        <div className="App">
-            <header className="container">
-                <div className="">
-                    <Header />
-                    <Routes>
-                        <Route
-                            path={handler["home"]["link"]}
-                            element={handler["home"]["route"]}
-                        />
-                        <Route
-                            path={handler["edit"]["link"] + ":id"}
-                            element={handler["edit"]["route"]}
-                        />
-                        <Route
-                            path={handler["comment"]["link"] + ":id"}
-                            element={handler["comment"]["route"]}
-                        />
-                        <Route
-                            path={handler["create"]["link"]}
-                            element={handler["create"]["route"]}
-                        />
-                        <Route
-                            path={handler["list"]["link"]}
-                            element={handler["list"]["route"]}
-                        />
-                    </Routes>
-                </div>
-            </header>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <header className="container">
+                    <div className="">
+                        <Header />
+                        <Routes>
+                            <Route
+                                path={handler["home"]["link"]}
+                                element={handler["home"]["route"]}
+                            />
+                            <Route
+                                path={handler["edit"]["link"] + ":id"}
+                                element={handler["edit"]["route"]}
+                            />
+                            <Route
+                                path={handler["comment"]["link"] + ":id"}
+                                element={handler["comment"]["route"]}
+                            />
+                            <Route
+                                path={handler["create"]["link"]}
+                                element={handler["create"]["route"]}
+                            />
+                            <Route
+                                path={handler["list"]["link"]}
+                                element={handler["list"]["route"]}
+                            />
+                        </Routes>
+                    </div>
+                </header>
+            </div>
+        </BrowserRouter>
     );
 }
 
